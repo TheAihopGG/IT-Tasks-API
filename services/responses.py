326ca27d4fe8aa1de6b2214773edb419b9d31963
TheAihopGG <108916224+TheAihopGG@users.noreply.github.com>
2024-12-27 10:05:33 +0700

    Added `ErrorResponse`

---
+++ services/responses.py
@@ -0,0 +1,18 @@
+from fastapi.responses import JSONResponse
+
+class ErrorResponse(JSONResponse):
+    def __init__(
+        self, *,
+        msg: str,
+        content = None,
+        status_code = 404,
+        headers = None,
+        media_type = None,
+        background = None
+    ):
+        content = {
+            "detail":{
+                "msg":msg
+            }
+        }
+        super().__init__(content, status_code, headers, media_type, background)
