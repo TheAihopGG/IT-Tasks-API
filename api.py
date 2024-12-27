872980c5f5586cc84e2263f5fed1215c8751cb87
TheAihopGG <108916224+TheAihopGG@users.noreply.github.com>
2024-12-27 10:06:12 +0700

    Added: - imports - `get_tasks_by_tags` - some validates

--- api.py
+++ api.py
@@ -1,57 +1,78 @@
 import aiosqlite
 import asyncio
+from services.responses import *
 from logging import *
 from services.database import create_tables, TASKS_COLUMNS
 from data.settings import *
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse, PlainTextResponse
 
 app = FastAPI()
 basicConfig(level=INFO)
-__doc__ = """
-    GET /api/get_task/?id=<id> - returns task with <id>
+__doc__ = '''
+    GET /api/task/ - returns task with <id>
     GET /api/get_tasks_ids - returns tasks ids
-"""
+'''
 
-@app.get("api/help")
+@app.get('api/help')
 async def help() -> PlainTextResponse:
+    # return
     return PlainTextResponse(__doc__)
 
 
-@app.get("/api/get_task")
-async def get_task(id: int) -> JSONResponse:
+@app.get('/api/task')
+async def get_task(request: Request) -> JSONResponse:
+    # get values from headers
+    id: int = request.headers.get('id', None)
+    # validate
+    if id < 0:
+        return ErrorResponse(f'Id must be bigger than 0')
+    # return
     async with aiosqlite.connect(DB_PATH) as db:
-        result = await (await db.execute(
-            "SELECT * FROM tasks WHERE id=?",
+        if task := await (await db.execute(
+            'SELECT * FROM tasks WHERE id=?',
             (id,)
-        )).fetchone()
-        if result:
+        )).fetchone():
             return JSONResponse(dict(zip(
                 TASKS_COLUMNS,
-                result
+                task
             )))
 
         else:
-            return JSONResponse({
-                "detail":{
-                    "msg":f"Task not found with id: {id}"
-                }
-            }, status_code=404)
+            return ErrorResponse(f'Task not found')
 
 
-@app.get("/api/get_tasks_ids")
+@app.get('/api/get_tasks_ids')
 async def get_tasks_ids() -> JSONResponse:
+    # just returns all tasks ids
     async with aiosqlite.connect(DB_PATH) as db:
-        result = await (await db.execute("SELECT id FROM tasks")).fetchall()
-        if result:
-            return JSONResponse({"ids":result[0]})
+        if tasks_ids := await (await db.execute('SELECT id FROM tasks')).fetchall():
+            return JSONResponse({'ids':tasks_ids[0]})
 
         else:
-            return JSONResponse({
-                "detail":{
-                    "msg":"No tasks available"
-                }
-            }, status_code=404)
+            return ErrorResponse('No tasks available')
 
-if __name__ == "__main__":
+
+@app.get('/api/task/by_tags')
+async def get_tasks_by_tags(request: Request) -> JSONResponse:
+    # get values from headers
+    tags: list[str] = request.headers.get('tags', None)
+    # validate
+    if not tags:
+        return ErrorResponse(f'Tags are required')
+    
+    elif not isinstance(tags, list):
+        return ErrorResponse(f'Tags must be array')
+    
+    elif len(tags) in range(1, MAX_REQUEST_TAGS_COUNT):
+        return ErrorResponse(f'Limited max tags count. The limit is: {MAX_REQUEST_TAGS_COUNT}')
+    # return
+    async with aiosqlite.connect(DB_PATH) as db:
+        if tasks := [dict(zip(TASKS_COLUMNS, task)) for task in await (await db.execute('SELECT * FROM tasks'))]:
+            if tasks_with_tag := [task if all(tag in task['tags'] for tag in tags) else None for task in tasks]:
+                return JSONResponse({'tasks':tasks_with_tag})
+            else:
+                return ErrorResponse(f'Tasks has not found with these tags: {tags}')
+
+if __name__ == '__main__':
     asyncio.run(create_tables())
