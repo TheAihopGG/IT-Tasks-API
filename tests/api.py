83ff7670742b6d7785cdb763138fb329fbf14a00
TheAihopGG <108916224+TheAihopGG@users.noreply.github.com>
2024-12-30 14:28:47 +0700

    fix: renamed test_get_ids

--- tests/api.py
+++ tests/api.py
@@ -3,12 +3,12 @@ import requests
 import rich
 import json
 
-API_DOMAIN = "http://127.0.0.1:8000"
+API_DOMAIN = 'http://127.0.0.1:8000'
 
 class ApiTestCase(unittest.TestCase):
     def stest_get_task(self):
         response = requests.get(
-            f"{API_DOMAIN}/api/task/",
+            f'{API_DOMAIN}/api/task/',
             headers={
                 'body':json.dumps(
                     {
@@ -19,24 +19,24 @@ class ApiTestCase(unittest.TestCase):
         )
         rich.print_json(response.content.decode())
 
-    def stest_get_ids(self):
+    def test_get_ids(self):
         response = requests.get(
-            f"{API_DOMAIN}/api/tasks/ids"
+            f'{API_DOMAIN}/api/tasks/ids'
         )
         rich.print_json(response.content.decode())
     
     def test_get_tasks_by_tags(self):
         response = requests.get(
-            f"{API_DOMAIN}/api/tasks/by_tags",
+            f'{API_DOMAIN}/api/tasks/by_tags',
             headers={
                 'body':json.dumps(
                     {
-                        'tags':["R121ust", "Python12"]
+                        'tags':['R121ust', 'Python12']
                     }
                 )
             }
         )
         rich.print_json(response.content.decode())
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     unittest.main()
